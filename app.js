6681c3e2f6837b7f64bbe97273d9d6cbb2f155df
Abdelrahman Habashy <abdelrahmanhabashy971@gmail.com>
2024-03-05 18:04:28 +0100

    remove console

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization")
     next()
 })
-console.log("dfdf")
+
 
 app.use("/shop",shopRoutes)
 app.use("/admin",adminRoutes)
